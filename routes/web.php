c258e34da68e23dff608135924ad99e2947f2a7b
Streetbell <ram@streetbell.com>
2026-09-01 13:05:45 +0530

    Add public downloads route

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use App\Controllers\Web\AuthController;
 use App\Controllers\Web\DashboardController;
 use App\Controllers\Web\DeviceController;
+use App\Controllers\Web\DownloadController;
 use App\Controllers\Web\MediaController;
 use App\Controllers\Web\PlaylistController;
 use App\Controllers\Web\ScheduleController;
@@ -12,6 +13,7 @@ use App\Controllers\Web\SettingsController;
 
 $router->get('/', [AuthController::class, 'login']);
 $router->get('/login', [AuthController::class, 'login']);
+$router->get('/downloads', [DownloadController::class, 'index']);
 $router->get('/dashboard', [DashboardController::class, 'index']);
 $router->get('/devices', [DeviceController::class, 'index']);
 $router->get('/media', [MediaController::class, 'index']);
