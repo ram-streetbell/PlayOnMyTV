345db8820bace95b4d86e963682b7958d96e8799
Streetbell <ram@streetbell.com>
2026-09-01 13:06:14 +0530

    Add application downloads link to header

--- app/Views/partials/header.php
+++ app/Views/partials/header.php
@@ -1,7 +1,7 @@
 <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
     <a class="navbar-brand col-lg-2 col-md-3 me-0 px-3 fs-6 text-uppercase" href="/dashboard">PlayOnMyTV</a>
-    <div class="navbar-nav flex-row d-none d-md-flex ms-auto px-3">
-        <span class="nav-link text-white-50">Admin Portal Scaffold</span>
+    <div class="navbar-nav flex-row d-none d-md-flex ms-auto px-3 align-items-center">
+        <a class="nav-link text-white" href="/downloads">Download Apps</a>
+        <span class="nav-link text-white-50">Admin Portal</span>
     </div>
 </header>
-
